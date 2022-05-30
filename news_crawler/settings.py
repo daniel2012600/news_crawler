29ac1a9a6458a21daeb9e7a3f5024847b5a10abd
bdaniel <bdaniel@ht.local>
2022-05-30 18:10:52 +0800

    add post api

--- news_crawler/settings.py
+++ news_crawler/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'news_crawler.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'news_crawler (+http://www.yourdomain.com)'
+# USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -37,10 +37,25 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cache-control": "max-age=0",
+    "cookie": " sso_token_king=53j0vsetp7ggg0kcwokog0wosgos4o840gw84c8gggss0koogc; GCLB=CLioy5abqMP1BA; sso_verify_king=3rsc24phf90kwgc8kccwc00w4g4w0gcsgww08osc448so80s0w; PHPSESSID=465b49bd30c6d78f5cdca85838f9c69b; BJYSESSION=465b49bd30c6d78f5cdca85838f9c69b; Hm_lvt_e5378fff1db5d9e5624629a74e0efbf0=1652932026,1653033372,1653636751,1653897543; Hm_lpvt_e5378fff1db5d9e5624629a74e0efbf0=1653900324; SERVERID=a12c271f06cd8fa195056fe7f3b58bbf|1653900328|1653900324 ",
+    "if-modified-since": "Wed, 27 Apr 2022 10:11:03 GMT",
+    "if-none-match": 'W/"626916b7-c6a"',
+    "referer": "https://www.zhibo16.live/index.php?g=&m=index&a=index",
+    "sec-ch-ua": '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
+    }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
@@ -62,9 +77,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'news_crawler.pipelines.NewsCrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'news_crawler.pipelines.NewsCrawlerPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- news_crawler/spiders/wz_news_api.py
+++ news_crawler/spiders/wz_news_api.py
@@ -1,17 +1,52 @@
 import scrapy
 import time # 引入time
+from scrapy.exceptions import CloseSpider
+import json
 
 class WzNewsApiSpider(scrapy.Spider):
     name = 'wz_news_api'
-    allowed_domains = ['pwa.zhibo16.live']
-    start_urls = [f'https://pwa.zhibo16.live/pwa-api/index.php?']
+    id_value = 31885
+
+    def start_requests(self):
+        time_params = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        url = f"https://pwa.zhibo16.live/pwa-api/index.php?g=H5&m=News&a=newsDetail&_={self.time_trans(time_params)}"
+        print(self.id_value)
+        yield scrapy.Request(url, method="POST", body=json.dumps({'id': self.id_value}) )
 
     def parse(self, response):
-        # g=H5&m=News&a=newsDetail&_={self.time_trans(time_params)}
 
-        pass
+        resource_json = response.json()
+        print("==============")
+        print(resource_json)
+        
+        if resource_json['data']['detail']['post_title'] != '':
+            # 用發布日期判斷新聞是否已經爬過了
+            publishtime = resource_json['data']['detail']['publishtime']
+            print("==============")
+            print(publishtime)
+            print(resource_json['data']['detail']['post_title'])
+            print(self.id_value)
+            print("==============")
+            # if time_trans(lastime_cawler_date) < time_trans(publishtime):
+                # data['id'] = self.id_value
+                # data['news_title'] = resource_json['data']['detail']['post_title']
+                # data['title_pic'] = resource_json['data']['detail']['smeta']['thumb']
+                # data['news_content'] = resource_json['data']['detail']['post_content']
+                # data['publish_date'] = publishtime
+                # news_data.append(data)
+            # else:
+            #     pass
+        else:
+            print(2222222222222222)
+            raise CloseSpider('close it')
+
+
+        self.id_value += 1
+        print(self.id_value)
+        print("==============")
+
     # 轉換日期格式至UNIX
-    def time_trans(qurey_date):
+    def time_trans(self, qurey_date):
         struct_time = time.strptime(qurey_date, "%Y-%m-%d %H:%M:%S") # 轉成時間元組
         time_stamp = int(time.mktime(struct_time)) # 轉成時間戳
         return time_stamp
